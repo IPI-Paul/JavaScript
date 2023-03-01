1cd8b3b97611df52440959618823948ad6e90624
Paul I Ighofose <paul@ipi-international.co.uk>
2023-03-01 07:33:36 +0000

    Delete Mosh_Hamedani_Angular directory

--- Mosh_Hamedani_Angular/README.md
+++
@@ -1,3 +0,0 @@
-# Angular Tutorial for Beginners- Learn Angular and TypeScript
-
-[Full Course](https://m.youtube.com/watch?v=k5E2AVpwsko).
--- Mosh_Hamedani_Angular/ts-hello/point.ts
+++
@@ -1,8 +0,0 @@
-
-export class Point {
-  constructor(private x?: number, private y?: number) {}
-
-  draw() {
-    console.log(`X: ${this.x}, Y: ${this.y}`);    
-  }
-}
